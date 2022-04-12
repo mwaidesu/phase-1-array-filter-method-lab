d675215398aed85073a56a2483b1807b9550c82d
Mwai <machariamwai@gmail.com>
2022-04-12 21:19:09 +0300

    Pass all tests

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(arr, names) {
+  return arr.filter(
+    (matchingNames) => matchingNames.toLowerCase() === names.toLowerCase()
+  );
+}
+
+function fuzzyMatch(arr, testString) {
+  return arr.filter(
+    (matchingNames) =>
+      matchingNames.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  );
+}
+
+function matchName(arr, namesName) {
+  return arr.filter((obj) => obj.name === namesName);
+}
